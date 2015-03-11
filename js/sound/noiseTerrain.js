2ee2b6b72baa6705e4edcb665829c70278d59f1d
Steve Minor <steve.e.minor@nordstrom.com>
2015-03-11 15:31:33 -0700

    removed some cruft from main visualization, html5 audio streams added

--- js/sound/noiseTerrain.js
+++ js/sound/noiseTerrain.js
@@ -1,29 +1,63 @@
-var client_id = '94fbf1dd918af5f7791d75d623fb4448';
+var client_id = '94fbf1dd918af5f7791d75d623fb4448',
+	context,
+	trackBuffer, 
+	source;
 
-SC.initialize({
-	client_id: client_id
-});
+window.addEventListener('load', init, false);
 
-SC.get('/users/infinityshred', function(info) {
-  console.log(info);
-});
+function init() {
+	try {
+		// Fix up for prefixing
+		window.AudioContext = window.AudioContext||window.webkitAudioContext;
+		context = new AudioContext();
+		source = context.createBufferSource();
 
-SC.get('/users/infinityshred/playlists/6281929', function(info) {
-	console.log(info);
-	info.tracks.forEach(function(track){
-		console.log(track);
-	});
+		SC.initialize({
+			client_id: client_id
+		});
 
-	//initializes some shitty flash player
-	// SC.stream(info.tracks[0].stream_url, {useHTML5Audio: true}, function(sound){
-	//	console.log(sound);
-	//	sound.play();
-	// });
+		SC.get('/users/infinityshred', function(info) {
+			console.log('info', info);
+		});
 
-	var track = document.getElementById('myTrack');
-	SC.get('/tracks/' + info.tracks[0].id, function(sound){
-		track.src = sound.stream_url + '?client_id=' + client_id;
-		// track.play();
-		console.log(sound);
-	});	
-});
+		SC.get('/users/infinityshred/playlists/6281929', function(info) {
+			console.log(info);
+			info.tracks.forEach(function(track){
+				console.log(track);
+			});
+
+			//initializes some shitty flash player
+			// SC.stream(info.tracks[0].stream_url, {useHTML5Audio: true}, function(sound){
+			//	console.log(sound);
+			//	sound.play();
+			// });
+
+			SC.get('/tracks/' + info.tracks[1].id, function(sound){
+				var xhr = new XMLHttpRequest();
+				xhr.open('GET', sound.stream_url + '?client_id=' + client_id, true);
+				xhr.responseType = 'arraybuffer';
+
+				xhr.onload = function(){
+					context.decodeAudioData(xhr.response, function(buffer){
+						// trackBuffer = buffer;
+						//is trackBuffer even necessary anymore?
+						source.buffer = buffer;
+						// source.buffer = trackBuffer;
+						source.connect(context.destination);
+						source.start(0);
+					});
+				};
+				xhr.send();
+				// context.decodeAudioData(sound, function(buffer){
+				// trackBuffer = buffer;
+				// });
+				// track.src = sound.stream_url + '?client_id=' + client_id;
+				// track.play();
+				console.log(sound);
+			});
+		});
+	}
+	catch(e) {
+		console.log('Web Audio API is not supported in this browser');
+	}
+}
